75ac57ffa31c610a5c419a2822f190b0c07945b1
maneki.nekko@gmail.com <maneki.nekko@gmail.com@213e0fc3-2fc3-359e-0eaa-521d5a0ba0bb>
2011-10-25 16:26:54 +0000

    ajout d'un debut de Dnd

--- trunk/src/solitaire/tasdecartes/PTasDeCartes.java
+++ trunk/src/solitaire/tasdecartes/PTasDeCartes.java
@@ -2,21 +2,33 @@ package solitaire.tasdecartes;
 
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DragGestureEvent;
+import java.awt.dnd.DragSource;
+
 import javax.swing.JPanel;
 import solitaire.carte.ICCarte;
 import solitaire.carte.PCarte;
+import solitaire.dnd.MyDragGestionListener;
+import solitaire.dnd.MyDragSourceListener;
 import solitaire.pac.Controleur;
 
-public class PTasDeCartes extends JPanel implements IPTasDeCartes{
+public class PTasDeCartes extends JPanel implements IPTasDeCartes {
 
 	private static final long serialVersionUID = 1L;
 	protected ICTasDeCartes controleur_;
 	public static int ecartDecompacte = 20;
 
+	// DnD
+	protected DragSource dragSource = null;
+	protected MyDragSourceListener myDragSourceListener = null;
+	
 	public PTasDeCartes(ICTasDeCartes controleur) {
 		super(null);
 		setSize(new Dimension(PCarte.largeur, PCarte.hauteur+ecartDecompacte*18));
 		controleur_ = controleur;
+		dragSource = new DragSource();
+		dragSource.createDefaultDragGestureRecognizer(this, DnDConstants.ACTION_MOVE, new MyDragGestionListener(this, dragSource));
 	}
 
 	@Override
