c1375ad65a54795a41a46f07b469cb0498163135
sketylee@gmail.com <sketylee@gmail.com@213e0fc3-2fc3-359e-0eaa-521d5a0ba0bb>
2011-10-31 17:25:20 +0000

    startng the drop method in MyDropTargetListener

---
+++ trunk/src/solitaire/dnd/MyDropTargetListener.java
@@ -0,0 +1,52 @@
+package solitaire.dnd;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetListener;
+import java.io.IOException;
+
+import solitaire.tasdecartes.PTasDeCartes;
+
+public class MyDropTargetListener implements DropTargetListener{
+
+	@Override
+	public void dragEnter(DropTargetDragEvent event) {
+		
+	}
+
+	@Override
+	public void dragExit(DropTargetEvent event) {
+		
+	}
+
+	@Override
+	public void dragOver(DropTargetDragEvent event) {
+		
+	}
+
+	@Override
+	public void drop(DropTargetDropEvent event) {
+		 Transferable transferable = event.getTransferable();
+         try {
+			if (transferable.isDataFlavorSupported(new DataFlavor(DataFlavor.javaJVMLocalObjectMimeType))) {
+			     PTasDeCartes pTasDeCarte = (PTasDeCartes) transferable.getTransferData(new DataFlavor(DataFlavor.javaJVMLocalObjectMimeType));
+			 }
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnsupportedFlavorException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void dropActionChanged(DropTargetDragEvent event) {
+		
+	}
+
+}
