afbcc6de2269ae6e5bb169c4b52654ebe69786c9
sketylee@gmail.com <sketylee@gmail.com@213e0fc3-2fc3-359e-0eaa-521d5a0ba0bb>
2011-11-04 16:44:10 +0000

    pas mal de modifs pour la disparition de la fenetre 'valise' a la fin d'un dnd

--- trunk/src/solitaire/dnd/MyDragGestureListener.java
+++ trunk/src/solitaire/dnd/MyDragGestureListener.java
@@ -7,23 +7,29 @@ import java.awt.dnd.DragGestureEvent;
 import java.awt.dnd.DragGestureListener;
 import java.awt.dnd.DragSource;
 import java.awt.dnd.DragSourceDragEvent;
+import java.awt.dnd.DragSourceDropEvent;
+import java.awt.dnd.DragSourceEvent;
+import java.awt.dnd.DragSourceListener;
 import java.awt.dnd.DragSourceMotionListener;
 import javax.swing.JPanel;
 import solitaire.carte.PCarte;
+import solitaire.tasdecartes.ICTasDeCartes;
 import solitaire.tasdecartes.PTasDeCartes;
 
 public class MyDragGestureListener implements DragGestureListener,
 		DragSourceMotionListener {
 	protected PCarte carteSelectionnee_ = null;
+	private ICTasDeCartes cTasDeCarte_;
 	private PTasDeCartes pTasDeCarte_;
 	protected DragGestureEvent theInitialEvent;
 	protected DragSource dragSource_;
 	protected MyDragSourceListener myDragSourceListener = null;
 	private Window valise_;
 
-	public MyDragGestureListener(PTasDeCartes pTasDeCarte, DragSource dragSource) {
+	public MyDragGestureListener(ICTasDeCartes cTasDeCarte, DragSource dragSource) {
 		dragSource_ = dragSource;
-		pTasDeCarte_ = pTasDeCarte;
+		cTasDeCarte_ = cTasDeCarte;
+		pTasDeCarte_ = (PTasDeCartes)cTasDeCarte_.getPresentation();
 		myDragSourceListener = new MyDragSourceListener(pTasDeCarte_);
 	}
 
@@ -31,7 +37,6 @@ public class MyDragGestureListener implements DragGestureListener,
 	public void dragGestureRecognized(DragGestureEvent event) {
 		carteSelectionnee_ = null;
 		try {
-			System.out.println(pTasDeCarte_.getLocation());
 			carteSelectionnee_ = (PCarte) pTasDeCarte_.getComponentAt(event.getDragOrigin().x, event.getDragOrigin().y-pTasDeCarte_.getY());
 		} catch (Exception e) {
 		}
@@ -57,6 +62,45 @@ public class MyDragGestureListener implements DragGestureListener,
 		if (valise_ != null) {
 			valise_.setLocation(1 + event.getX(), 1 + event.getY());
 		}
+	}
+	
+	class MyDragSourceListener implements DragSourceListener{
+
+		private JPanel jpanel_;
+
+		public MyDragSourceListener(JPanel jpanel) {
+			jpanel_ = jpanel;
+		}
+		
+		@Override
+		public void dragDropEnd(DragSourceDropEvent event) {
+			System.out.println("fin du drag?");
+			valise_.setVisible(false);
+
+			if ( event.getDropSuccess() ){
+				// TODO verifier la source a retirer !!
+				jpanel_.remove((Component)event.getSource());
+				jpanel_.validate();
+				jpanel_.repaint();
+			}
+		}
+
+		@Override
+		public void dragEnter(DragSourceDragEvent dsde) {
+		}
+
+		@Override
+		public void dragExit(DragSourceEvent dse) {
+		}
+
+		@Override
+		public void dragOver(DragSourceDragEvent dsde) {
+		}
+
+		@Override
+		public void dropActionChanged(DragSourceDragEvent dsde) {
+		}
+
 	}
 
 }
