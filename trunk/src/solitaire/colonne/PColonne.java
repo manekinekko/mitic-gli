0f38f06e036fe8c1ef8de40ed82adcce2a010cbe
sketylee@gmail.com <sketylee@gmail.com@213e0fc3-2fc3-359e-0eaa-521d5a0ba0bb>
2011-11-09 13:28:22 +0000

    correction du bug lors du retournement du sommet du tas cache

--- trunk/src/solitaire/colonne/PColonne.java
+++ trunk/src/solitaire/colonne/PColonne.java
@@ -16,13 +16,16 @@ public class PColonne extends PDoubleTas implements IPColonne{
 	
 	private static final long serialVersionUID = 1L;
 	private ICColonne controleur_;
+	private PTasDeCartes pCache_;
+	private PTasDeCartes pVisible_;
 
 	public PColonne(final ICColonne controleur, final ICTasDeCartes cachees, final ICTasDeCartes visible) {
 		super(controleur, cachees, visible);
 		controleur_ = controleur;
 		
-		PTasDeCartes pCache = (PTasDeCartes)cachees.getPresentation();
-		pCache.setLocation(0, 0);
+		pCache_ = (PTasDeCartes)cachees.getPresentation();
+		pVisible_ = (PTasDeCartes)visible.getPresentation();
+		pCache_.setLocation(0, 0);
 		
 		DropTarget dropTarget_ = new DropTarget(this, new MyDropTargetListener(cTasVisible_));
 		
@@ -54,7 +57,9 @@ public class PColonne extends PDoubleTas implements IPColonne{
 				if(visible.isVide()){
 					try {
 						visible.empiler(cachees.getSommet());
-						visible.depiler();
+						cachees.depiler();
+						repositionner();
+						pVisible_.decompacter();
 					} catch (Exception e1) {
 						e1.printStackTrace();
 					}
@@ -76,8 +81,7 @@ public class PColonne extends PDoubleTas implements IPColonne{
 	}
 	
 	public void repositionner(){
-		PTasDeCartes pVisible = (PTasDeCartes)cTasVisible_.getPresentation();
-		pVisible.setLocation(0, cTasCache_.getNombre()*PTasDeCartes.ecartDecompacte);
+		pVisible_.setLocation(0, cTasCache_.getNombre()*PTasDeCartes.ecartDecompacte);
 	}
 
 }
