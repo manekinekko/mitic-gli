dc686f5975f369e0caf70c0ab4e281ce5c899fb6
sketylee@gmail.com <sketylee@gmail.com@213e0fc3-2fc3-359e-0eaa-521d5a0ba0bb>
2011-11-12 13:13:18 +0000

    déplacement et renommage du package observer

--- src/Observer/Feedbackable.java
+++
@@ -1,9 +0,0 @@
-package Observer;
-
-import java.awt.Point;
-
-public interface Feedbackable {
-	public void highlightValideDrop(Point location);
-	public void highlightInvalideDrop(Point location);
-	public void clearHighlight();
-}
