15b108cefebbbd777a1769cb8a5ce54135831c51
sketylee@gmail.com <sketylee@gmail.com@213e0fc3-2fc3-359e-0eaa-521d5a0ba0bb>
2011-11-27 00:59:49 +0000

    ajout de doc pour le package solitaire

--- src/solitaire/usinedecontroleurs/UsineDeControleurs.java
+++ src/solitaire/usinedecontroleurs/UsineDeControleurs.java
@@ -12,9 +12,9 @@ import solitaire.tasdecartesalternees.CTasDeCartesAlternees;
 import solitaire.tasdecartescolorees.CTasDeCartesColorees;
 
 /**
- * Redefinit l'usine du noyau applicatif. Toutes les methodes sont surchargées
+ * Redéfinit l'usine du noyau applicatif. Toutes les méthodes sont surchargées
  * afin de renvoyer un contrôleur, héritant de l'abstraction, au lieu de
- * l'habituelle abstraction.
+ * l'abstraction elle même.
  * 
  * @author Wassim Chegham {@link contact@cheghamwassim.com}
  * @author Gurval Le Bouter {@link sketylee@gmail.com}
@@ -22,43 +22,68 @@ import solitaire.tasdecartescolorees.CTasDeCartesColorees;
  */
 public class UsineDeControleurs extends Usine {
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public CCarte newCarte(int valeur, int couleur) {
 		return new CCarte(valeur, couleur);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public CColonne newColonne(String nom, Usine usine) {
 		return new CColonne(nom, usine);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public CDoubleTas newDoubleTas(Tas cachees, Tas visibles) {
 		return new CDoubleTas(cachees, visibles);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public CSabot newSabot(String nom, Usine usine) {
 		return new CSabot(nom, usine);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public CSolitaire newSolitaire(String nom, Usine usine) {
 		return new CSolitaire(nom, usine);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public CTasDeCartes newTasDeCartes(String nom, Usine usine) {
 		return new CTasDeCartes(nom, usine);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public CTasDeCartesAlternees newTasDeCartesAlternees(String nom, Usine usine) {
 		return new CTasDeCartesAlternees(nom, usine);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public CTasDeCartesColorees newTasDeCartesColorees(String nom, int couleur, Usine usine) {
 		return new CTasDeCartesColorees(nom, couleur, usine);
 	}
 }
+
